6695ef678124fd96e7e0b18d580d896c45a86807
Seo <seohyun6361@gmail.com>
2025-08-13 01:18:01 +0900

    ✨[feat]#1 - 검색뷰 컴포넌트 제작

---
+++ Muje/Muje/Modules/Home/Views/SearchView/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  Muje
+//
+//  Created by 김서현 on 8/13/25.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
