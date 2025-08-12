c8f7ab368a99183640530b5f31a4de8984002a88
Seo <seohyun6361@gmail.com>
2025-08-13 01:20:47 +0900

    🔧[Chore]#1 SearchBar 컴포넌트 수정

--- Muje/Muje/Modules/Home/Views/SearchView/Component/SearchBar.swift
+++ Muje/Muje/Modules/Home/Views/SearchView/Component/SearchBar.swift
@@ -2,17 +2,38 @@
 //  SearchBar.swift
 //  Muje
 //
-//  Created by 김서현 on 8/13/25.
+//  Created by 김서현 on 8/12/25.
 //
+
+// FIXME: 폰트, 컬러 수정
 
 import SwiftUI
 
 struct SearchBar: View {
+    @Binding var typingStatus: Bool
+    @Binding var searchText: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            HStack {
+                Image(systemName: "magnifyingglass")
+                Text(
+                    typingStatus
+                     ? searchText
+                     : "제목, 검색어"
+                )
+                Spacer()
+            }
+            .padding(.vertical, 11)
+            .padding(.horizontal, 11.5)
+            .background(
+                RoundedRectangle(cornerRadius: 100)
+                    .fill(Color.white)
+            )
+            .frame(maxWidth: .infinity)
+            .padding(.trailing, 13)
+            Text("취소")
+        }
+        .frame(maxWidth: .infinity)
     }
-}
-
-#Preview {
-    SearchBar()
 }
